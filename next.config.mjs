3c513617ec38f22583800fe732e04746f3492440
imposglobal <rushikesh@imposglobal.com>
2024-09-24 18:21:27 +0530

    Update

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,10 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    output: 'export',
-    images: { unoptimized: true }
+    images: {
+        formats: ['image/avif', 'image/webp'], // Keep image optimization formats
+        domains: ['https://doodlodesigns.com'], // Optional: Define allowed domains for external images
+    }
 };
 
 export default nextConfig;
