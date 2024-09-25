ca9bc1233e5268e03d3bb0617b77e2e9933457d3
imposglobal <rushikesh@imposglobal.com>
2024-09-25 12:04:56 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,7 @@ const nextConfig = {
     experimental: {
         optimizeCss: true, // Enable CSS optimization
     },
-    compiler: {
-        styledComponents: true,
-      },
+  
     output: 'export',
     images: {
         unoptimized: true,
@@ -16,14 +14,7 @@ const nextConfig = {
         deviceSizes: [640, 768, 1024, 1280, 1600], // Responsive image sizes
         imageSizes: [16, 32, 48, 64, 96], // Sizes for next/image
     },
-    webpack: (config) => {
-        // Additional optimizations can be added here
-        config.optimization.splitChunks = {
-          chunks: 'all',
-          maxSize: 200000, // Split large chunks for smaller bundles
-        };
-        return config;
-      },
+  
     };
     
     export default nextConfig;
