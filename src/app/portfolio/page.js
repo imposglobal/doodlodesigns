2d8bdd384d0df2bd109e97e1b2c25df3ac747657
imposglobal <rushikesh@imposglobal.com>
2024-10-09 12:17:59 +0530

    update portfolio

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -1,15 +1,13 @@
 "use client";
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Logo from "../Images/logo";
 import Menu from "../Component/Menu";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import {useState, useEffect} from 'react';
-import BrandAnimated from "../Component/Brandlineanimation";
+import { useState, useEffect } from 'react';
 import WorkAnimation from "../Component/WorkAnimated";
-import Arrows from "../Component/Arrowanimation";
 import WorkAnimationlr from "../Component/WorkAnimatedLR";
 
 const Portfolio = () => {
@@ -17,35 +15,34 @@ const Portfolio = () => {
     document.title = "Portfolio - Doodlo Designs Studio";
   }, []);
 
-    useEffect(() =>{
-        AOS.init({
-          duration: 1000, // Customize the duration of the animations
-          once: false,     // Whether animation should happen only once - while scrolling down
-        });
-      }, []);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Customize the duration of the animations
+      once: false,     // Whether animation should happen only once - while scrolling down
+    });
+  }, []);
    
-    const [inactive, setInactive] = useState(true); // Set initial state to true
-  
-    const toggleClass = () => {
-      setInactive(!inactive);
-    };
+  const [inactive, setInactive] = useState(true); // Set initial state to true
 
-    const portfolioItems = [
-      {no:"01", url:"/portfolio/swiggy-x-the-bowl-company",  id: 1, title: "SWIGGY + BOWL COMPANY", category: "Packaging Design | Marketing collaterals | Advertising ", imageUrl: "/home/portfolio/1.png" },
-      {no:"02", url:"/portfolio/coinsafe-vpn",  id: 2, title: "COINSAFE VPN", category: "Branding | Website UI | Web Development | Application | Marketing Collaterals", imageUrl: "/home/portfolio/csafe.gif" },
-      {no:"03", url:"/portfolio/tarabai",  id: 3, title: "TARABAI", category: "Branding | Packaging | Retail Branding | Marketing Collaterals | Social Media", imageUrl: "/home/portfolio/5.png" },
-      {no:"04", url:"/portfolio/blackb-finance-services",  id: 4, title: "BLACK B FINANCE", category: "Branding | Website UI | Web Development", imageUrl: "/home/portfolio/blackb.gif" },
-      {no:"05", url:"portfolio/yolo-mart",  id: 5, title: "YOLOMART", category: "Branding | Retail Branding | Marketing Collateral | Packaging | Website UI | Web Development | Social Media", imageUrl: "/home/portfolio/yolomart.png" },
-      {no:"06", url:"/portfolio/mahatee",  id: 6, title: "MAHATEE INTERACTIVES", category: "Branding | Website UI | Shopify Development | Ecommerce Setup | Illustrations | Publication & Book Design | Social Media", imageUrl: "/home/portfolio/mahatee.gif" },
-      {no:"07", url:"portfolio/clickrnd",  id: 7, title: "Click R & D", category: "Branding | Website UI | Social Media", imageUrl: "/home/portfolio/clickrnd.jpg" },
-      {no:"08", url:"portfolio/citibank",  id: 8, title: "CITIBANK", category: "Illustrations | Presentation | Wall Graffiti", imageUrl: "/home/portfolio/6.png" },
-      {no:"09", url:"/portfolio/nutribee",  id: 9, title: "Nutribee", category: "Illustration | Packaging Design", imageUrl: "/home/portfolio/10.png" },
-      {no:"10", url:"portfolio/flash-card-illustration-and-packaging",  id: 10, title: "Littile learners trove", category: "Branding | Illustrations | Product Design | Packaging Design | Social Media", imageUrl: "/home/portfolio/12.png" },
-      {no:"11", url:"/portfolio/coffee-table-book",  id: 11, title: "Wuerth Industries", category: "Publication - Coffee Table Book Design | Corporate Giftings", imageUrl: "/home/portfolio/8.png" },
-      {no:"12", url:"/portfolio/royal-weddings",  id: 12, title: "Royal Wedding ", category: "Branding | Business Collaterals", imageUrl: "/home/portfolio/11.png" },
-      {no:"12", url:"/portfolio/alhidaya",  id: 4, title: "Alhidaya", category: "Branding | Website UI | Web Development", imageUrl: "/home/portfolio/alhidaya.png" },
-      
-      // Add more items as needed
+  const toggleClass = () => {
+    setInactive(!inactive);
+  };
+
+  const portfolioItems = [
+    { url: "/portfolio/alhidaya", id: 13, title: "Alhidaya", category: "Branding | Website UI | Web Development", imageUrl: "/home/portfolio/alhidaya.png" },
+    { url: "/portfolio/swiggy-x-the-bowl-company", id: 1, title: "SWIGGY + BOWL COMPANY", category: "Packaging Design | Marketing collaterals | Advertising ", imageUrl: "/home/portfolio/1.png" },
+    { url: "/portfolio/coinsafe-vpn", id: 2, title: "COINSAFE VPN", category: "Branding | Website UI | Web Development | Application | Marketing Collaterals", imageUrl: "/home/portfolio/csafe.gif" },
+    { url: "/portfolio/tarabai", id: 3, title: "TARABAI", category: "Branding | Packaging | Retail Branding | Marketing Collaterals | Social Media", imageUrl: "/home/portfolio/5.png" },
+    { url: "/portfolio/blackb-finance-services", id: 4, title: "BLACK B FINANCE", category: "Branding | Website UI | Web Development", imageUrl: "/home/portfolio/blackb.gif" },
+    { url: "portfolio/yolo-mart", id: 5, title: "YOLOMART", category: "Branding | Retail Branding | Marketing Collateral | Packaging | Website UI | Web Development | Social Media", imageUrl: "/home/portfolio/yolomart.png" },
+    { url: "/portfolio/mahatee", id: 6, title: "MAHATEE INTERACTIVES", category: "Branding | Website UI | Shopify Development | Ecommerce Setup | Illustrations | Publication & Book Design | Social Media", imageUrl: "/home/portfolio/mahatee.gif" },
+    { url: "portfolio/clickrnd", id: 7, title: "Click R & D", category: "Branding | Website UI | Social Media", imageUrl: "/home/portfolio/clickrnd.jpg" },
+    { url: "portfolio/citibank", id: 8, title: "CITIBANK", category: "Illustrations | Presentation | Wall Graffiti", imageUrl: "/home/portfolio/6.png" },
+    { url: "/portfolio/nutribee", id: 9, title: "Nutribee", category: "Illustration | Packaging Design", imageUrl: "/home/portfolio/10.png" },
+    { url: "portfolio/flash-card-illustration-and-packaging", id: 10, title: "Littile learners trove", category: "Branding | Illustrations | Product Design | Packaging Design | Social Media", imageUrl: "/home/portfolio/12.png" },
+    { url: "/portfolio/coffee-table-book", id: 11, title: "Wuerth Industries", category: "Publication - Coffee Table Book Design | Corporate Giftings", imageUrl: "/home/portfolio/8.png" },
+    { url: "/portfolio/royal-weddings", id: 12, title: "Royal Wedding ", category: "Branding | Business Collaterals", imageUrl: "/home/portfolio/11.png" },
+  
   ];
 
   // State to track the number of items to display
@@ -53,189 +50,112 @@ const Portfolio = () => {
 
   // Function to load more items
   const loadMore = () => {
-      setVisibleItems(prevCount => prevCount + 6);
+    setVisibleItems(prevCount => prevCount + 6);
   };
 
-    return (
-      <div>
-        <div className={styles.portfolio}>
-            {/* Menu start */}
-     <div id="wrapper">
-      <div className={`menuwrap ${inactive ? 'inactive' : ''} doodbg`}>
-        <div className="menusec">
-          <div className="menurow">
-            <div className="mcol1">
-            <div className="menuitem">
-              <Link href="/" className="mitem ">
-                Home
-              </Link>
-             
-            </div>
-
-            <div className="menuitem">
-              <Link href="/portfolio" className="mitem active">
-                Portfolio
-              </Link>
-              <div className="border-bottom"></div>
-            </div>
-
-            <div className="menuitem">
-              <Link href="/services" className="mitem">
-                Our Services
-              </Link>
-            </div>
-
-            <div className="menuitem">
-              <Link href="/our-story" className="mitem">
-                Our Story
-              </Link>
-            </div>
-
-            <div className="menuitem">
-              <Link href="/reach-us" className="mitem">
-                Reach us
-              </Link>
-            </div>
-
-            <div className="menuitem">
-              <Link href="https://doodlocomics.com/" className="mitem">
-                Shop
-              </Link>
-            </div>
-
-            </div>
-            <div className="mcol2">
-              <div className="socialsec">
-                <p className="gtt">Get in touch </p>
-                <Link href="https://in.linkedin.com/company/doodlodesign">
-                                    <Image
-                                    className="msocial"
-                                    src="/home/social/flinked.svg"
-                                    alt="Next.js Logo"
-                                    layout="responsive"
-                                    width={100}
-                                    height={37}
-                                    priority
-                                    />
-                                    </Link>
-                                    <Link href="https://www.instagram.com/doodlo_design/">
-                                    <Image
-                                    className="msocial"
-                                    src="/home/social/finsta.svg"
-                                    alt="Next.js Logo"
-                                    layout="responsive"
-                                    width={100}
-                                    height={37}
-                                    priority
-                                    />
-                                    </Link>
-                                    <Link href="https://www.linkedin.com/company/13436743">
-                                    <Image
-                                    className="msocial"
-                                    src="/home/social/ffb.svg"
-                                    alt="Next.js Logo"
-                                    layout="responsive"
-                                    width={100}
-                                    height={37}
-                                    priority
-                                    />
-                                    </Link>
+  return (
+    <div>
+      <div className={styles.portfolio}>
+        {/* Menu start */}
+        <div id="wrapper">
+          <div className={`menuwrap ${inactive ? 'inactive' : ''} doodbg`}>
+            <div className="menusec">
+              <div className="menurow">
+                <div className="mcol1">
+                  <div className="menuitem">
+                    <Link href="/" className="mitem ">Home</Link>
+                  </div>
+                  <div className="menuitem">
+                    <Link href="/portfolio" className="mitem active">Portfolio</Link>
+                    <div className="border-bottom"></div>
+                  </div>
+                  <div className="menuitem">
+                    <Link href="/services" className="mitem">Our Services</Link>
+                  </div>
+                  <div className="menuitem">
+                    <Link href="/our-story" className="mitem">Our Story</Link>
+                  </div>
+                  <div className="menuitem">
+                    <Link href="/reach-us" className="mitem">Reach us</Link>
+                  </div>
+                  <div className="menuitem">
+                    <Link href="https://doodlocomics.com/" className="mitem">Shop</Link>
+                  </div>
+                </div>
+                <div className="mcol2">
+                  <div className="socialsec">
+                    <p className="gtt">Get in touch </p>
+                    <Link href="https://in.linkedin.com/company/doodlodesign">
+                      <Image className="msocial" src="/home/social/flinked.svg" alt="LinkedIn" layout="responsive" width={100} height={37} priority />
+                    </Link>
+                    <Link href="https://www.instagram.com/doodlo_design/">
+                      <Image className="msocial" src="/home/social/finsta.svg" alt="Instagram" layout="responsive" width={100} height={37} priority />
+                    </Link>
+                    <Link href="https://www.linkedin.com/company/13436743">
+                      <Image className="msocial" src="/home/social/ffb.svg" alt="Facebook" layout="responsive" width={100} height={37} priority />
+                    </Link>
+                  </div>
+                </div>
               </div>
+            </div>
+            <div className="mcopy">
+              <p>© 2024 Doodlo | All Rights Reserved | Terms of Use | Privacy Policy</p>
             </div>
           </div>
         </div>
-        <div className="mcopy">
-        <p>© 2024 Doodlo | All Rights Reserved | Terms of Use | Privacy Policy</p>
-        </div>
-      </div>
-    </div>
-    {/* Menu end */}
-       
+        {/* Menu end */}
         <div className={`${styles.row} ${styles.mobrow}`}>
-        <div className={styles.column1}>
+          <div className={styles.column1}>
             <div className={`${styles.logosec} ${styles.mlsec}`}>
-            <Logo />
+              <Logo />
             </div>
-        </div>
-        <div className={styles.column2}>
+          </div>
+          <div className={styles.column2}>
             <div className={styles.logosec}>
-            <Menu className={styles.menuicon} onClick={toggleClass} />
+              <Menu className={styles.menuicon} onClick={toggleClass} />
             </div>
-        </div>
+          </div>
         </div>
         <div className={styles.booknow}>
-            <div className={styles.bookwrap}>
+          <div className={styles.bookwrap}>
             <WorkAnimation />
-            </div>
-            <div className={styles.bookwrap1}>
-            <WorkAnimation />
-            </div>
           </div>
-          {/* Porfolio Section */}
-          
-        </div>
-        <div className={styles.portsec}>
-            <div className={styles.grid}>
-              <div className={styles.texta}>
-              <h2 data-aos="fade-up" className={styles.title}>Our 
-              <Image data-aos="fade-up"
-            className={styles.homestar}
-            src="/home/homestar.png"
-            alt="Next.js Logo"
-            layout="responsive"
-            width={100}
-            height={37}
-            priority
-            onClick={toggleClass}
-          />
-              </h2>
-              <h2 data-aos="fade-up" className={styles.sec_heading}>Portfolio</h2>
-              </div>
-              <div>
-              
-              </div>
-                {portfolioItems.slice(0, visibleItems).map((item, index) => (
-                    <div id={item.id}
-                        key={item.id}
-                        className={`${styles.card}`}
-                    >
-                      <h2 data-aos="fade-up" className={styles.titles}>{item.no} </h2>
-                      <Link className={styles.hurl} href={item.url}>
-                        <img data-aos="fade-up" src={item.imageUrl} alt={item.title} className={styles.image} />
-                        <div data-aos="fade-up" className={styles.textContainer}>
-                            <h2 className={styles.itemTitle}>{item.title}</h2>
-                            <p className={styles.category}>{item.category}</p>
-                        </div>
-                        </Link>
-                    </div>
-                ))}
-            </div>
-            {visibleItems < portfolioItems.length && (
-                <button className={styles.loadMore} onClick={loadMore}>
-                    Load More
-                </button>
-            )}
-        </div>
-          {/* Lets Discuss section */}
-       <div data-aos="fade-up" className={`${styles.pointer}`}>
-        <div className={styles.discuss_row}>
-        <h2 className={styles.mainheadingdis}>Ready to Take Your</h2>
-          <h2 className={styles.sec_headingdis}>Brand to the Next Level?</h2>
-          <p className={styles.disdesc}>Contact Doodlo Design Studio today for your brand consultation!</p>
-          <Link href="/reach-us#calendly" className="boton_elegante mtbtn">Schedule a Free Consultation</Link>
-          <div className={styles.booknows}>
-            <div className={styles.bookwraps}>
-            <WorkAnimationlr />
-            </div>
-            <div className={styles.bookwraps1}>
-            <WorkAnimationlr />
-            </div>
+          <div className={styles.bookwrap1}>
+            <WorkAnimation />
           </div>
         </div>
+        {/* Portfolio Section */}
       </div>
-      {/* Lets Discuss section End */}
+      <div className={styles.portsec}>
+        <div className={styles.grid}>
+          <div className={styles.texta}>
+            <h2 data-aos="fade-up" className={styles.title}>Our 
+              <Image data-aos="fade-up" className={styles.homestar} src="/home/homestar.png" alt="Star" layout="responsive" width={100} height={37} priority onClick={toggleClass} />
+            </h2>
+            <h2 data-aos="fade-up" className={styles.sec_heading}>Portfolio</h2>
+          </div>
+          <div></div>
+          {portfolioItems.slice(0, visibleItems).map((item, index) => (
+            <div id={item.id} key={item.id} className={`${styles.card}`}>
+              <h2 data-aos="fade-up" className={styles.titles}>{String(index + 1).padStart(2, '0')}</h2>
+              <Link className={styles.hurl} href={item.url}>
+                <img data-aos="fade-up" src={item.imageUrl} alt={item.title} className={styles.image} />
+                <div data-aos="fade-up" className={styles.textContainer}>
+                  <h2 className={styles.itemTitle}>{item.title}</h2>
+                  <p className={styles.category}>{item.category}</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+        {visibleItems < portfolioItems.length && (
+          <button className={styles.loadMore} onClick={loadMore}>
+            Load More
+          </button>
+        )}
       </div>
-    );
+    </div>
+  );
 };
 
 export default Portfolio;
